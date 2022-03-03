3f1ff744c7ad2e25344a3af1f03800284cc61834
Teliakshay313 <akshayteli2021@gmail.com>
2022-03-03 10:04:20 +0530

    second commit

--- Junitdemo/src/com/progs/Code1.java
+++ Junitdemo/src/com/progs/Code1.java
@@ -2,12 +2,12 @@ package com.progs;
 
 public class Code1 {
 	
-	public int divide(int x,int y)
+	public int divide(int a,int b)
 	{
 		// WAP to return the divide of x/y 
 		int res =0;
-		if(y==0)
-		res=x/y;
+		if(b==0)
+		res=a/b;
 		return res;
 		
 	}
